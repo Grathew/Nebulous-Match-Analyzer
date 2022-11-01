dee7b12c63cafcc0837fd3e7af44dd33042b1183
Grathew <harberinger@gmail.com>
2022-11-01 14:29:40 -0400

    Reformatting the imports to work in a more logical way.

--- main.py
+++ main.py
@@ -28,6 +28,7 @@ except:
     
     print("Please follow prompts.")
     SettingsDict["NebulousPath"] = input("Please enter the path to Nebulous Fleet Command: ")
+    SettingsDict["OutputPath"] = input("Please enter the path to the folder you want this program to save things: ")
     SettingsDict["PlayerName"] = input("Please enter your player name: ").lower()
     print("Answer the following quesiton with yes or no.")
     SettingsDict["MatchJson"] = input("Would you like to save your logs as json: ").lower()
@@ -48,13 +49,16 @@ except:
 if SettingsDict["DebugMode"] == "yes":
     print("SettingsDict")
     pprint(SettingsDict)
+    print("\n")
 
 SavesPath = SettingsDict["NebulousPath"] + "\\saves"
 if os.path.exists(SavesPath):
     if SettingsDict["DebugMode"] == "yes":
         print("debugging file search")
-        print(SavesPath)
+        print("Path we are looking from: " + SavesPath)
+        print("\n")
         print("walking though saves looking for files")
+        print("\n")
         for(root, dirs, file) in os.walk(SavesPath):
             for f in file:
                 if '.fleet' in f:
@@ -65,61 +69,81 @@ if os.path.exists(SavesPath):
                     print("Ship found " + f)
                 if '.xml' in f:
                     print("Report found " + f)
+        print("\n")
         print("Check and see if it has found everything you'd expect. If not yell at Grathew. He's on the Nebulous Discord or the Git.")
+        print("\n")
 else:
     print("Could not find required file tree. Please check your settings file and varify your Nebulous location.")
 
-
 #open and read Fleets
-FleetPath = SavesPath + "\\Fleets"
-if os.path.exists(FleetPath):
-    FleetFolder = os.walk(FleetPath)
-    FleetFiles = []
-    for root, dirs, files in FleetFolder:
-        for name in files:
-            if name.endswith('.fleet'):
-                FleetFiles.append(name)
-else:
-    print("Could not find the Fleets folder. Please check your Nebulous\\Saves for the Fleets folder.") 
+if SettingsDict["FleetJson"] == "yes":
+    FleetPath = SavesPath + "\\Fleets"
+    if os.path.exists(FleetPath):
+        FleetFolder = os.walk(FleetPath)
+        FleetFiles = []
+        for root, dirs, files in FleetFolder:
+            for name in files:
+                if name.endswith('.fleet'):
+                    FleetFiles.append(name)
+        if SettingsDict["DebugMode"] == "yes":
+            print("Fleets")
+            pprint(FleetFiles)
+            print("\n")
+    else:
+        print("Could not find the Fleets folder. Please check your Nebulous\\Saves for the Fleets folder.") 
     
-    #open and read Missle Templates
-MissilePath = SavesPath + "\\MissileTemplates"
-if os.path.exists(MissilePath):
-    MissileFolder = os.listdir(MissilePath)
-    MissileFiles = []
-    for f in range(len(MissileFolder)):
-        if MissileFolder[f].endswith('.missile'):
-            MissileFiles.append(MissileFolder[f])
-else:
-    print("Could not find the MissileTemplates folder. Please check your Nebulous\\Saves for the MissileTemplates folder.") 
+#open and read Missle Templates
+if SettingsDict["MissileJson"] == "yes":
+    MissilePath = SavesPath + "\\MissileTemplates"
+    if os.path.exists(MissilePath):
+        MissileFolder = os.listdir(MissilePath)
+        MissileFiles = []
+        for f in range(len(MissileFolder)):
+            if MissileFolder[f].endswith('.missile'):
+                MissileFiles.append(MissileFolder[f])
+        if SettingsDict["DebugMode"] == "yes":
+            print("Missiles")
+            pprint(MissileFiles)
+            print("\n")
+    else:
+        print("Could not find the MissileTemplates folder. Please check your Nebulous\\Saves for the MissileTemplates folder.") 
 
 #open and read ships
-ShipPath = SavesPath + "\\ShipTemplates"
-if os.path.exists(ShipPath):
-    ShipFolder = os.listdir(ShipPath)
-    ShipFiles = []
-    for f in range(len(ShipFolder)):
-        if ShipFolder[f].endswith('.ship'):
-            ShipFiles.append(ShipFolder[f])
-else:
-    print("Could not find the Fleets folder. Please check your Nebulous\\Saves for the ShipTemplates folder.")
-    
+if SettingsDict["ShipJson"] == "yes":
+    ShipPath = SavesPath + "\\ShipTemplates"
+    if os.path.exists(ShipPath):
+        ShipFolder = os.listdir(ShipPath)
+        ShipFiles = []
+        for f in range(len(ShipFolder)):
+            if ShipFolder[f].endswith('.ship'):
+                ShipFiles.append(ShipFolder[f])
+        if SettingsDict["DebugMode"] == "yes":
+            print("Ships")
+            pprint(ShipFiles)
+            print("\n")
+    else:
+        print("Could not find the Fleets folder. Please check your Nebulous\\Saves for the ShipTemplates folder.")
+        
 #open and read Battle Reports
-ReportPath = SavesPath + "\\SkirmishReports"
-if os.path.exists(ReportPath):
-    ReportFolder = os.listdir(ReportPath)
-    ReportFiles = []
-    for f in range(len(ReportFolder)):
-        if ReportFolder[f].endswith('.xml'):
-            ReportFiles.append(ReportFolder[f])
-else:
-    print("Could not find the Fleets folder. Please check your Nebulous\\Saves for the SkirmishReports folder.")
+if SettingsDict["MatchJson"] == "yes":
+    ReportPath = SavesPath + "\\SkirmishReports"
+    if os.path.exists(ReportPath):
+        ReportFolder = os.listdir(ReportPath)
+        ReportFiles = []
+        for f in range(len(ReportFolder)):
+            if ReportFolder[f].endswith('.xml'):
+                ReportFiles.append(ReportFolder[f])
+        if SettingsDict["DebugMode"] == "yes":
+            print("Reports")   
+            pprint(ReportFiles)
+            print("\n")
+    else:
+        print("Could not find the SkirmishReports folder. Please check your Nebulous\\Saves for the SkirmishReports folder.")
 
-print("Fleets")
-pprint(FleetFiles)
-print("Missiles")
-pprint(MissileFiles)
-print("Ships")
-pprint(ShipFiles)
-print("Reports")   
-pprint(ReportFiles)
+if os.path.exists(SettingsDict["OutputPath"]):
+    print("\n")
+    print("Output location found. Continuing...")
+    print("\n")
+else:
+    os.mkdir(SettingsDict["OutputPath"]) 
+
